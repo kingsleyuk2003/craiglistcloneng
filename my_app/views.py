9081cd8ce00801485f3d89a57e7289bab240e112
kingsleyuk2003 <kingsleyuk2003@yahoo.com>
2021-07-25 00:37:45 +0100

    changes

--- my_app/views.py
+++ my_app/views.py
@@ -5,3 +5,9 @@ from django.shortcuts import render
 
 def home(request):
     return render(request,'my_app/base.html')
+
+
+def new_search(request):
+    search = request.POST.get('search')
+    context = {'search':search}
+    return render(request,'my_app/new_search.html',context)
